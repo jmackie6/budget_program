461b52990d1e15b2e487f8a6fd3b6d98ec422450
Jared <mackiejared66@gmail.com>
2016-01-22 17:06:06 -0700

    made some changes to assignemnt

--- handle_form.php
+++ handle_form.php
@@ -1,11 +1,18 @@
 <?php
+	$name     = filter_input(INPUT_POST, 'name',     FILTER_SANITIZE_STRING);
+	$email    = filter_input(INPUT_POST, 'email',    FILTER_VALIDATE_EMAIL );
+	$major    = filter_input(INPUT_POST, 'major',    FILTER_SANITIZE_STRING);
+	$visited  = filter_input(INPUT_POST, 'visited',  FILTER_SANITIZE_STRING, FILTER_REQUIRE_ARRAY);
+	$comments = filter_input(INPUT_POST, 'comments', FILTER_SANITIZE_STRING);
+
 	$name=$_REQUEST['name'];
 	$email=$_REQUEST['email'];
 	$major=$_REQUEST['major'];
 	$comments=$_REQUEST['comments'];
+	echo "<h1>Greetings, $name </h1>";
 
 	echo "<p>Name: $name</p>";
-	echo "<p>Email: $email</p>";
+	echo "<a href='mailto:$email?Subject=HandleForm' target='_top'>$email</a>";
 	echo "<p>Major: $major</p>";
 
 	if(!empty($_POST['visited']))
