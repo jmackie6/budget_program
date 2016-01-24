0989012e02ff441e0c23e4cee11b8fb8a0b6ae9d
Jared <mackiejared66@gmail.com>
2016-01-23 23:22:11 -0700

    More changes to css and php file

--- survey.php
+++ survey.php
@@ -75,7 +75,7 @@ $data_results = $data;
 $data_results = file_get_contents('survey.txt');
 
 $tempArray = json_decode($data_results);
-echo count($game) . "<br>";
+echo $tempArray."<br>";
 
 
 foreach($tempArray as $item) 
