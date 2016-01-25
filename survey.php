0954527840e4ecaf93c878785eb739a81eaba67f
Jared <mackiejared66@gmail.com>
2016-01-25 06:12:42 -0700

    trying to do the seesions

--- survey.php
+++ survey.php
@@ -11,10 +11,7 @@
     	<br>
 <?php
     	
-//  if (isset($_POST['submit'])) { 
-// $_session['game'] = $_POST['game'];
-// $_session['party'] = $_POST['party'];
-// } 
+
 
 $game     = filter_input(INPUT_POST, 'game',     FILTER_SANITIZE_STRING);
 $party    = filter_input(INPUT_POST, 'party',    FILTER_VALIDATE_EMAIL );
@@ -29,26 +26,37 @@ $card=$_REQUEST['card'];
 $strategy=$_REQUEST['strategy'];
 $coop=$_REQUEST['coop'];
 $dice=$_REQUEST['dice'];
+
+session_start()
+	 if (isset($_POST['submit'])) { 
+	$_session['game'] = $_POST['game'];
+	$_session['party'] = $_POST['party'];
+	$_session['card'] = $_POST['card'];
+	$_session['strategy'] = $_POST['strategy'];
+	$_session['coop'] = $_POST['coop'];
+	$_session['dice'] = $_POST['dice'];
+
+	} 
+	
 echo "<h2>Votes</h2>";
 
 $data = array("game" => $game, "party" => $party, "card" => $card, "strategy" => $strategy, "coop" => $coop, "dice" => $dice);
 //$data_results = $data;
-?>
-<table style="margin: auto">
-    		<colgroup>
-        		<col span="6" style="background-color:#A3BCC4">
+
+ // <table style="margin: auto">
+ //    		<colgroup>
+ //        		<col span="6" style="background-color:#A3BCC4">
        
-      		</colgroup>
-      		<tr>
-            	<th>game</th>
-             	<th>party</th>
-             	<th>card</th>
-             	<th>strategy</th>
-             	<th>coop</th>
-             	<th>dice</th>
-      		</tr>
-		</table> -->
-<?php
+ //      		</colgroup>
+ //      		<tr>
+ //            	<th>game</th>
+ //             	<th>party</th>
+ //             	<th>card</th>
+ //             	<th>strategy</th>
+ //             	<th>coop</th>
+ //             	<th>dice</th>
+ //      		</tr>
+ // 		</table> --> -->
 
 // $tempArray1[] = $data;
 // if (isset($data)) {
@@ -63,14 +71,14 @@ $data = array("game" => $game, "party" => $party, "card" => $card, "strategy" =>
 
 
 
-               echo "<tr>";
-                  echo "<td>"."$game"."</td>" . "                ";
-                  echo "<td>"."$party"."</td>";
-                  echo "<td>"."$card"."</td>";
-                  echo "<td>"."$strategy"."</td>";
-                  echo "<td>"."$coop"."</td>";
-                  echo "<td>"."$dice"."</td>";
-              echo "</tr>". "<br>";
+              //  echo "<tr>";
+              //     echo "<td>"."$game"."</td>" . "                ";
+              //     echo "<td>"."$party"."</td>";
+              //     echo "<td>"."$card"."</td>";
+              //     echo "<td>"."$strategy"."</td>";
+              //     echo "<td>"."$coop"."</td>";
+              //     echo "<td>"."$dice"."</td>";
+              // echo "</tr>". "<br>";
 // $json = json_encode($tempArray1);
 
 $data_results = file_get_contents('survey.txt');
@@ -79,18 +87,17 @@ $tempArray = json_decode($data_results);
 
 
 
-// foreach($tempArray as $item) 
-//       {
-//   				  //echo "Votes: ";
-
-//                   echo "<span>$item->game</span>";
-//                   echo $item->party . "  ";
-//                   echo $item->card . "  ";
-//                   echo $item->strategy . "  ";
-//                   echo $item->coop . "  ";
-//                   echo $item->dice . "  ";
-//                   echo "<br><br>";
-//       }
+foreach($tempArray as $item) 
+      {
+  		
+                  echo $item->game. "<br> ";
+                  echo $item->party . " <br> ";
+                  echo $item->card . " <br> ";
+                  echo $item->strategy . " <br> ";
+                  echo $item->coop . " <br> ";
+                  echo $item->dice . "<br>  ";
+                  echo "<br><br>";
+      }
 // foreach($tempArray as $item) 
 //       {
 //       		echo "<br>";
@@ -110,21 +117,10 @@ $jsonData = json_encode($tempArray);
 
 file_put_contents('survey.txt', $jsonData);  
 
-//echo "$game"."<br>"."  $party"."<br>". "  $card"."<br>". "  $strategy"."<br>". "  $coop"."<br>". "  $dice"."<br>";
-echo "<span>.".$game. "</span>". "  "."  $party"."  ". "  $card"."  ". "  $strategy"."  ". "  $coop"."  ". "  $dice"."  ";
+echo "$game"."<br>"."  $party"."<br>". "  $card"."<br>". "  $strategy"."<br>". "  $coop"."<br>". "  $dice"."<br>";
+//echo "<br>".$game. "<br>  "."  $party"." <br> ". "  $card"." <br> ". "  $strategy"."<br>  ". "  $coop"." <br> ". "  $dice"."<br>  ";
 echo "<br>";
 
-foreach($tempArray as $item) 
-      {
-  				  
-                  echo $item->game;
-                  echo $item->party . "  ";
-                  echo $item->card . "  ";
-                  echo $item->strategy . "  ";
-                  echo $item->coop . "  ";
-                  echo $item->dice . "  ";
-                  echo "<br>";
-      }
 ?>
 		<a href= "survey.html" class="button"> 
             <p class="a">Go back To Question Page</p> 
