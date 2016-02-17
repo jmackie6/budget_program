29ffea35444539576fbd7910e207fff78aef284a
Jared <mackiejared66@gmail.com>
2016-02-16 17:25:48 -0700

    trying more things to get it to work

--- dbconnect.php
+++ dbconnect.php
@@ -1,11 +1,18 @@
 <?php
 
+// define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
+// define('DB_PORT',getenv('OPENSHIFT_MYSQL_DB_PORT')); 
+// define('DB_USER',getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
+// define('DB_PASS',getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
+// define('DB_NAME',getenv('OPENSHIFT_GEAR_NAME'));
 
+// $dsn = 'mysql:dbname='.DB_NAME.';host='.DB_HOST.';port='.DB_PORT;
+// $dbh = new PDO($dsn, DB_USER, DB_PASS);
 define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
 define('DB_PORT', getenv('OPENSHIFT_MYSQL_DB_PORT'));
 define('DB_USER', getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
 define('DB_PASS', getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
-define('DB_NAME', getenv('OPENSHIFT_GEAR_NAME'));
+define('DB_NAME', getenv('OPENSHIFT_APP_NAME'));
 
 $dbhost = constant("DB_HOST"); // Host name 
 $dbport = constant("DB_PORT"); // Host port
