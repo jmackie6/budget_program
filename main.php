bac810515b857a1e435ed26690ec16394a279c75
Jared <mackiejared66@gmail.com>
2016-02-16 22:37:55 -0700

    trying to fo more things ot get it to work

--- main.php
+++ main.php
@@ -14,9 +14,29 @@ if(isset($_POST['btn-login']))
  $row=mysqli_fetch_array($res);
 
 
- echo $_SESSION['user'];
- echo $email;
- echo $upass;
+echo $_SESSION['user'];
+echo $email;
+echo $upass;
+
+define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
+define('DB_PORT', getenv('OPENSHIFT_MYSQL_DB_PORT'));
+define('DB_USER', getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
+define('DB_PASS', getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
+define('DB_NAME', getenv('OPENSHIFT_APP_NAME'));
+
+
+
+$dbhost = constant("DB_HOST"); // Host name 
+$dbport = constant("DB_PORT"); // Host port
+$dbusername = constant("DB_USER"); // Mysql username 
+
+$dbpassword = constant("DB_PASS"); // Mysql password 
+//echo $dbpassword;
+$db_name = constant("DB_NAME"); // Database name 
+//echo $db_name;
+
+$mysqlCon = mysqli_connect($dbhost, $dbusername, $dbpassword, $db_name, $dbport) or die("Error: " . mysqli_error($mysqlCon));
+mysqli_select_db($mysqlCon, $db_name) or die("Error: " . mysqli_error($mysqlCon));
 
  if($row['password']==md5($upass))
  {
