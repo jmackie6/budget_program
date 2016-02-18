000039743e44add38c6a5726cabedd2f606bea4b
Jared <mackiejared66@gmail.com>
2016-02-17 22:57:32 -0700

    trying more things to get it to work

--- main.php
+++ main.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-//include_once 'dbconnect.php';
+include_once 'dbconnect.php';
 
 if(isset($_SESSION['user'])!="")
 {
@@ -8,35 +8,11 @@ if(isset($_SESSION['user'])!="")
 }
 if(isset($_POST['btn-login']))
 {
- $email = mysqli_real_escape_string($_POST['email']);
- $upass = mysqli_real_escape_string($_POST['pass']);
- $res=mysqli_query("SELECT * FROM user WHERE email='$email'");
- $row=mysqli_fetch_array($res);
+ $email = mysqli_real_escape_string($mysqlCon, $_POST['email']);
+ $upass = mysqli_real_escape_string($mysqlCon, $_POST['pass']);
+ $res=mysqli_query($mysqlCon, "SELECT * FROM user WHERE email='$email'");
+ $row=mysqli_fetch_array($mysqlCon, $res);
 
-
-echo $_SESSION['user'];
-echo $email;
-echo $upass;
-
-define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
-define('DB_PORT', getenv('OPENSHIFT_MYSQL_DB_PORT'));
-define('DB_USER', getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
-define('DB_PASS', getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
-define('DB_NAME', getenv('OPENSHIFT_APP_NAME'));
-
-
-
-$dbhost = constant("DB_HOST"); // Host name 
-$dbport = constant("DB_PORT"); // Host port
-$dbusername = constant("DB_USER"); // Mysql username 
-
-$dbpassword = constant("DB_PASS"); // Mysql password 
-//echo $dbpassword;
-$db_name = constant("DB_NAME"); // Database name 
-//echo $db_name;
-
-$mysqlCon = mysqli_connect($dbhost, $dbusername, $dbpassword, $db_name, $dbport) or die("Error: " . mysqli_error($mysqlCon));
-mysqli_select_db($mysqlCon, $db_name) or die("Error: " . mysqli_error($mysqlCon));
 
  if($row['password']==md5($upass))
  {
