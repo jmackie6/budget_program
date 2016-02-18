8e490d2dea982b89c4cf1ffc2a2758d314b1ba29
Jared <mackiejared66@gmail.com>
2016-02-17 17:49:28 -0700

    uploading some more files

--- addIncome.php
+++ addIncome.php
@@ -42,6 +42,7 @@
        
       <?php
         
+        
 define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
 define('DB_PORT', getenv('OPENSHIFT_MYSQL_DB_PORT'));
 define('DB_USER', getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
@@ -53,7 +54,6 @@ $dbport = constant("DB_PORT"); // Host port
 $dbusername = constant("DB_USER"); // Mysql username 
 $dbpassword = constant("DB_PASS"); // Mysql password 
 $db_name = constant("DB_NAME"); // Database name 
-
 
 $mysqlCon = mysqli_connect($dbhost, $dbusername, $dbpassword, "", $dbport) or die("Error: " . mysqli_error($mysqlCon));
 mysqli_select_db($mysqlCon, $db_name) or die("Error: " . mysqli_error($mysqlCon));
