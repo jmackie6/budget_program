c0076ee7fea11ba8eed0f5b3138da2aa682e5490
Jared <mackiejared66@gmail.com>
2016-02-18 21:13:38 -0700

    making more chages to get it working

--- addExpenses.php
+++ addExpenses.php
@@ -81,6 +81,7 @@ mysqli_select_db($mysqlCon, $db_name) or die("Error: " . mysqli_error($mysqlCon)
 VALUES ('$userRow[user_id]', '$food','$rent','$health_insurance','$car_insurance', '$utilities', '$other', '$month')";
 
 
+
 if (mysqli_query($mysqlCon, $sql) === TRUE) {
     echo "New record created successfully";
 } else {
